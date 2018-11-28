5b0be0e22793b9e413638478b1f866caf06e3857
vitkuz <vitkuz.info@gmail.com>
2018-11-29 00:43:46 +0300

    First

--- src/server/routes/api/v1/index.js
+++ src/server/routes/api/v1/index.js
@@ -2,12 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const UsersRouter = require('./users/index');
-const handleError404 = require('../../../middlewares/error404');
-const handleError500 = require('../../../middlewares/error500');
 
 router.use('/', UsersRouter);
-
-router.use(handleError404); //handle 404 error
-router.use(handleError500); //handle 500 error
 
 module.exports = router;
--- src/server/startup/routes.js
+++ src/server/startup/routes.js
@@ -1,6 +1,13 @@
 const usersApiRoutes = require('../routes/api/v1/users');
 
+const handleError404 = require('../middlewares/error404');
+const handleError500 = require('../middlewares/error500');
+
 module.exports = function (app) {
     app.use('/api/v1', usersApiRoutes);
+
+    app.use(handleError404); //handle 404 error
+    app.use(handleError500); //handle 500 error
+
     return app;
 };
